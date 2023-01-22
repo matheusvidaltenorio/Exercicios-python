1d944ecbc67c42b6e727488b76e5a509c2b39fed
matheusvidaltenorio <matheustenoriovidal@gmail.com>
2023-01-21 22:38:51 -0300

    005-antecessor e sucessor.

---
+++ Exercicíos-Python/005-suc_e_ante.py
@@ -0,0 +1,4 @@
+num = int(input('Digite um número: '))
+sucessor = num+1
+antecessor = num-1
+print('O número base é {}\nSeu Antecessor é {}\nE seu Sucessor é {}.'.format(num, antecessor, sucessor))
