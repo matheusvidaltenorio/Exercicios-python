813fa0428e009d2956a6358c2d08b069c3e573f7
matheusvidaltenorio <matheustenoriovidal@gmail.com>
2023-01-24 20:31:14 -0300

    Update 008-metros_para_cent_mili.py
    Resolvendo todas as medidas.

--- Exercicíos-Python/008-metros_para_cent_mili.py
+++ Exercicíos-Python/008-metros_para_cent_mili.py
@@ -1,5 +1,11 @@
 valor = int(input('Digite o valor em metros: '))
+km = valor/1000
+hec = valor/100 
+dec = valor/10
+deci = valor*10
 cent = valor*100
 mili = valor*1000
-print('O valor base é {}.'.format(valor), end=' ')
-print('Transformando em Centimetros fica {} e em milimetros fica {}.'.format(cent, mili))
+print('O valor base é {}.'.format(valor))
+print('Transformando em Km fica {} e em decametros fica {}.'.format(km, hec))
+print('Transformando em Hectometros fica {} e em decimetros fica {}.'.format(dec, deci))
+print('Transformando em Centimetros fica {:.0f} e em milimetros fica {:.0f}.'.format(cent, mili))
