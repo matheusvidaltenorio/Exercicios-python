6fc22b4b19744991e1446081c77c4b2731b418b9
matheusvidaltenorio <matheustenoriovidal@gmail.com>
2023-01-24 19:29:17 -0300

    007-media_aluno.py

---
+++ Exercicíos-Python/007-media_aluno.py
@@ -0,0 +1,4 @@
+n1 = int(input('Digite sua primeira nota: '))
+n2 = int(input('Digite sua segunda nota: '))
+m = (n1+n2)/2
+print('A notas do aluno foram {} e {}, e sua média é {}'.format(n1, n2, m))
