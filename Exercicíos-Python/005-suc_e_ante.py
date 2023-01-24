da2a8a7695691a050699ad83c44323c02551559f
matheusvidaltenorio <matheustenoriovidal@gmail.com>
2023-01-24 19:34:33 -0300

    Update 005-suc_e_ante.py
    Melhorando o jeito de realizar a lógica do programa.

--- Exercicíos-Python/005-suc_e_ante.py
+++ Exercicíos-Python/005-suc_e_ante.py
@@ -1,4 +1,2 @@
 num = int(input('Digite um número: '))
-sucessor = num+1
-antecessor = num-1
-print('O número base é {}\nSeu Antecessor é {}\nE seu Sucessor é {}.'.format(num, antecessor, sucessor))
+print('O número base é {}. Seu Antecessor é {}. E seu Sucessor é {}.'.format(num, (num - 1), (num + 1)))
