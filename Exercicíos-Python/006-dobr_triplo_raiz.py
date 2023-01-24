b56170f81a2418f949365a8c992e6384d8d60c3e
matheusvidaltenorio <matheustenoriovidal@gmail.com>
2023-01-24 19:29:00 -0300

    006-dobr_triplo_raiz.py

---
+++ Exercicíos-Python/006-dobr_triplo_raiz.py
@@ -0,0 +1,9 @@
+num = int(input('Digite um número: '))
+dobro = num * 2
+triplo = num * 3
+raiz = num**(1/2)
+print('='*20)
+print('O dobro de {} é {}.'.format(num, dobro))
+print('O Triplo de {} é {}.'.format(num, triplo))
+print('A raiz de {} é {}.'.format(num, raiz))
+print('='*20)
