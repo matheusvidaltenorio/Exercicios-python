2088c8ac71c658e6b61f78988f525c80fcf8c9c2
matheusvidaltenorio <matheustenoriovidal@gmail.com>
2023-01-24 19:30:05 -0300

    011-calculo_parede.py

---
+++ Exercicíos-Python/011-calculo_parede.py
@@ -0,0 +1,4 @@
+altura = int(input('Digite a altura da parede: '))
+largura = int(input('Digite a largura da parede: '))
+resultado = (altura*largura)/2
+print('A quantidade de tinta que irá ser usado é {}L'.format(resultado))
