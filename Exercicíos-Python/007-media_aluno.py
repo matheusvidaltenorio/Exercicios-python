e90fc73e305a7a0024d75506a9b88c9837ae7b63
matheusvidaltenorio <matheustenoriovidal@gmail.com>
2023-01-24 20:31:03 -0300

    Update 007-media_aluno.py
    troca de int para Float.

--- Exercicíos-Python/007-media_aluno.py
+++ Exercicíos-Python/007-media_aluno.py
@@ -1,4 +1,4 @@
-n1 = int(input('Digite sua primeira nota: '))
-n2 = int(input('Digite sua segunda nota: '))
+n1 = float(input('Digite sua primeira nota: '))
+n2 = float(input('Digite sua segunda nota: '))
 m = (n1+n2)/2
-print('A notas do aluno foram {} e {}, e sua média é {}'.format(n1, n2, m))
+print('A notas do aluno foram {} e {}, e sua média é {:.1f}'.format(n1, n2, m))
