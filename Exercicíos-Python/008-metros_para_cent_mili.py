10a519424b7ab02ad984b73abf305436a9104b6d
matheusvidaltenorio <matheustenoriovidal@gmail.com>
2023-01-24 19:29:32 -0300

    008-metros_para_cent_mili.py

---
+++ Exercicíos-Python/008-metros_para_cent_mili.py
@@ -0,0 +1,5 @@
+valor = int(input('Digite o valor em metros: '))
+cent = valor*100
+mili = valor*1000
+print('O valor base é {}.'.format(valor), end=' ')
+print('Transformando em Centimetros fica {} e em milimetros fica {}.'.format(cent, mili))
