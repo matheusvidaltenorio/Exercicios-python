36f5569425965c772a7f6cc23fb92cd854bdf1fe
matheusvidaltenorio <matheustenoriovidal@gmail.com>
2023-01-31 01:45:38 -0300

    020-aluno_sorteio_em_ordem.py

---
+++ Exercicíos-Python/020-aluno_sorteio_em_ordem.py
@@ -0,0 +1,8 @@
+import random
+a = str(input('Primeiro aluno: '))
+b = str(input('Segundo aluno: '))
+c = str(input('Terceiro aluno: '))
+d = str(input('Quarto aluno: '))
+lista = [a, b, c, d]
+random.shuffle(lista)
+print('O aluno escolhi foi: {} '.format(lista))
